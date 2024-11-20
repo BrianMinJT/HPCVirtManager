44b500da6054b9aded7d0766faf6e0e0f2e93976
BrianMinJT <jtmin88@gmail.com>
2024-11-20 11:34:51 -0800

    New UI update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+import React from "react";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import NotFound from "./pages/NotFound";
 import { ProtectedRoutes } from "./routes/ProtectedRoutes";
 import Forbidden from "./pages/Forbidden";
 import Login from "./pages/Login";
-import PasswordForget from "./pages/PasswordForget"
+import PasswordForget from "./pages/PasswordForget";
 import { ConfirmDialog } from "primereact/confirmdialog";
+import { BreadcrumbProvider } from "./context/BreadcrumbContext";
 
 function App() {
   return (
-    <div className="App">
-      <ConfirmDialog />
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/forget-password" element={<PasswordForget />} />
-          <Route path="/404" element={<NotFound />} />
-          <Route path="/403" element={<Forbidden />} />
-        </Routes>
-        <ProtectedRoutes />
-      </HashRouter>
-    </div>
+    <HashRouter>
+      <BreadcrumbProvider>
+        <div className="App">
+          <ConfirmDialog />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/forget-password" element={<PasswordForget />} />
+            <Route path="/404" element={<NotFound />} />
+            <Route path="/403" element={<Forbidden />} />
+          </Routes>
+          <ProtectedRoutes />
+        </div>
+      </BreadcrumbProvider>
+    </HashRouter>
   );
 }
 
